71ef1a84609f8d6712f09580c0e8710fd3d888a6
monkeysun555 <ls3817@nyu.edu>
2019-12-06 14:04:06 -0500

    train auto entropy and lr

--- batch_file/static_rl.sh
+++ batch_file/static_rl.sh
@@ -2,7 +2,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=2
 #SBATCH --cpus-per-task=4
-#SBATCH --time=24:00:00
+#SBATCH --time=30:00:00
 #SBATCH --mem=4GB
 #SBATCH --job-name=chunk_rl_2s
 #SBATCH --mail-type=END
